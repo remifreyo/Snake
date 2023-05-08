1577cb3abd9bac3052ed7995decb05b8071e4094
Jeremy Cox <jeremy@theoutlet.org>
2023-05-08 16:38:31 -0400

    move snake head and food succesfully

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 /*----- constants -----*/
-
+const board = document.querySelector('.board')
 /*----- state variables -----*/
-
+let foodRow = Math.floor(Math.random() * 25) + 1
+let foodColumn = Math.floor(Math.random() * 25) + 1
+let headRow = Math.floor(Math.random() * 15) + 5
+let headColumn = Math.floor(Math.random() * 15) + 5
+let snakeFood = `<div class ="food" style="grid-area: ${foodRow} / ${foodColumn}"></div>`
+let snakeHead = `<div class ="head" style="grid-area: ${headRow} / ${headColumn}"></div>`
 /*----- cached elements  -----*/
 
 /*----- functions -----*/
+const init = () => {
+  board.innerHTML = snakeFood + snakeHead
+}
 const moveSnake = (e) => {
   console.log(e)
 }
+init()
 
 /*----- event listeners -----*/
 document.addEventListener('keydown', moveSnake)
