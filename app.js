798ed09f9561afa62e4714a043b9560099335664
Jeremy Cox <jeremy@theoutlet.org>
2023-05-10 13:10:17 -0400

    update score and append snake body to head

--- app.js
+++ app.js
@@ -1,61 +1,117 @@
 /*----- constants -----*/
 const board = document.querySelector('.board')
+const hTag = document.querySelector('h1')
 /*----- state variables -----*/
 let foodRow = Math.floor(Math.random() * 25) + 1
 let foodColumn = Math.floor(Math.random() * 25) + 1
 let headRow = Math.floor(Math.random() * 15) + 5
 let headColumn = Math.floor(Math.random() * 15) + 5
-let score
-let highscore
+let score = 0
+let highscore = 0
 let snakeFood
-let snakeHead
+let snakeBody = [{ x: headRow, y: headColumn }]
 let horizontalDirection = 0
 let verticalDirection = 0
+let gameIsOver = false
 /*----- cached elements  -----*/
 
 /*----- functions -----*/
 
 const moveSnake = (e) => {
-  if (e.key === 'ArrowLeft') {
+  if (e.key === 'ArrowLeft' && horizontalDirection !== 1) {
     horizontalDirection = -1
     verticalDirection = 0
   }
-  if (e.key === 'ArrowUp') {
+  if (e.key === 'ArrowUp' && verticalDirection !== 1) {
     verticalDirection = -1
     horizontalDirection = 0
   }
-  if (e.key === 'ArrowRight') {
+  if (e.key === 'ArrowRight' && horizontalDirection !== -1) {
     horizontalDirection = 1
     verticalDirection = 0
   }
-  if (e.key === 'ArrowDown') {
+  if (e.key === 'ArrowDown' && verticalDirection !== -1) {
     verticalDirection = 1
     horizontalDirection = 0
   }
   init()
 }
-const init = () => {
-  snakeFood = `<div class ="food" style="grid-area: ${foodRow} / ${foodColumn}"></div>`
-  snakeHead = `<div class ="head" style="grid-area: ${headRow} / ${headColumn}"></div>`
-  if (headRow > 1 && headRow < 25) {
-    headRow += verticalDirection
-  } else if (headRow === 1) {
-    headRow += 1
-  } else if (headRow === 25) {
-    headRow += -1
+const gameOver = (e) => {
+  if (e.target.innerText === 'Play Again!') {
+    location.reload()
   }
-  if (headColumn > 1 && headColumn < 25) {
-    headColumn += horizontalDirection
-  } else if (headColumn === 1) {
-    headColumn += 1
-  } else if (headColumn === 25) {
-    headColumn += -1
-  }
-
-  board.innerHTML = snakeFood + snakeHead
 }
+
+const collideWithSelf = () => {}
+
+const moveApple = () => {
+  foodRow = Math.floor(Math.random() * 25) + 1
+  foodColumn = Math.floor(Math.random() * 25) + 1
+}
+const init = () => {
+  if (gameIsOver === false) {
+    snakeFood = `<div class ="food" style="grid-area: ${foodRow} / ${foodColumn}"></div>`
+
+    for (i = snakeBody.length - 1; i >= 0; i--) {
+      if (snakeBody[0].x >= 1 && snakeBody[0].x <= 25) {
+        snakeBody[i].x += verticalDirection
+      } else if (snakeBody[0].x < 1 || snakeBody[0].x > 25) {
+        gameIsOver = true
+      }
+      if (snakeBody[0].y >= 1 && snakeBody[0].y <= 25) {
+        snakeBody[i].y += horizontalDirection
+      } else if (snakeBody[0].y < 1 || snakeBody[0].y > 25) {
+        gameIsOver = true
+      }
+    }
+    let snake = ''
+    snakeBody.forEach((square) => {
+      snake += `<div class="head" style="grid-area: ${square.x} / ${square.y}"></div>`
+    })
+    if (snakeBody[0].y === foodColumn && snakeBody[0].x === foodRow) {
+      if (verticalDirection === 1) {
+        snakeBody.push({
+          x: snakeBody[snakeBody.length - 1].x - 1,
+          y: snakeBody[snakeBody.length - 1].y
+        })
+      }
+      if (verticalDirection === -1) {
+        snakeBody.push({
+          x: snakeBody[snakeBody.length - 1].x + 1,
+          y: snakeBody[snakeBody.length - 1].y
+        })
+      }
+      if (horizontalDirection === -1) {
+        snakeBody.push({
+          x: snakeBody[snakeBody.length - 1].x,
+          y: snakeBody[snakeBody.length - 1].y + 1
+        })
+      }
+      if (horizontalDirection === 1) {
+        snakeBody.push({
+          x: snakeBody[snakeBody.length - 1].x,
+          y: snakeBody[snakeBody.length - 1].y - 1
+        })
+      }
+      moveApple()
+      score++
+    }
+    board.innerHTML = snakeFood + snake
+    document.querySelector('button').style.visibility = 'hidden'
+    document.querySelector('#score').innerHTML = 'Score:' + ' ' + score
+  } else {
+    hTag.style.color = 'red'
+    hTag.innerText = 'Game Over'
+    if (score > highscore) {
+      document.querySelector('#high-score').innerHTML =
+        'Highest Score:' + ' ' + score
+    }
+    document.querySelector('button').style.visibility = 'visible'
+  }
+}
+
 setInterval(init, 170)
 
 /*----- event listeners -----*/
 document.addEventListener('keydown', moveSnake)
-document.addEventListener('click', init)
+document.addEventListener('click', gameOver)
